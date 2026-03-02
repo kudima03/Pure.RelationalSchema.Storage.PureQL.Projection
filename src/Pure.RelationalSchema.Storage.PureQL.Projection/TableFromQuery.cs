2006c9b32e1f4edb5f60b81fb0de6364f533270b
Dmitry Kurochkin <kudima03@gmail.com>
2026-03-02 17:23:11 +0300

    TableFromQuery added

---
+++ src/Pure.RelationalSchema.Storage.PureQL.Projection/TableFromQuery.cs
@@ -0,0 +1,31 @@
+using Pure.Primitives.Abstractions.String;
+using Pure.Primitives.String;
+using Pure.RelationalSchema.Abstractions.Column;
+using Pure.RelationalSchema.Abstractions.Index;
+using Pure.RelationalSchema.Abstractions.Table;
+using PureQL.CSharp.Model;
+
+namespace Pure.RelationalSchema.Storage.PureQL.Projection;
+
+internal sealed record TableFromQuery : ITable
+{
+    public TableFromQuery(Query query)
+        : this(new EmptyString(), new ColumnsFromQuery(query.SelectExpressions), []) { }
+
+    internal TableFromQuery(
+        IString name,
+        IEnumerable<IColumn> columns,
+        IEnumerable<IIndex> indexes
+    )
+    {
+        Name = name;
+        Columns = columns;
+        Indexes = indexes;
+    }
+
+    public IString Name { get; }
+
+    public IEnumerable<IColumn> Columns { get; }
+
+    public IEnumerable<IIndex> Indexes { get; }
+}
