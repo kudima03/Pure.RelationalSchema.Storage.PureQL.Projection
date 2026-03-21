fa2464f6d57516e744fc92e2af00d87a7ac7f3d3
kudima03 <kudima03@gmail.com>
2026-03-21 13:53:54 +0300

    Support all field types in SELECT projection

--- src/Pure.RelationalSchema.Storage.PureQL.Projection/RowsFromDatasets.cs
+++ src/Pure.RelationalSchema.Storage.PureQL.Projection/RowsFromDatasets.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Linq.Expressions;
 using Pure.RelationalSchema.Abstractions.Column;
+using Pure.RelationalSchema.Abstractions.ColumnType;
 using Pure.RelationalSchema.ColumnType;
 using Pure.RelationalSchema.HashCodes;
 using Pure.RelationalSchema.Storage.Abstractions;
 using PureQL.CSharp.Model;
+using PureQL.CSharp.Model.ArrayReturnings;
+using PureQL.CSharp.Model.Returnings;
 using String = Pure.Primitives.String.String;
 
 namespace Pure.RelationalSchema.Storage.PureQL.Projection;
@@ -39,6 +42,8 @@ internal sealed record RowsFromDatasets : IQueryable<IRow>
         Query query
     )
     {
+        List<IStoredSchemaDataSet> datasetList = datasets.ToList();
+
         IEnumerable<string> reversedPath = query
             .From.Entity.Split(".")
             .AsEnumerable()
@@ -47,12 +52,20 @@ internal sealed record RowsFromDatasets : IQueryable<IRow>
         string tableName = reversedPath.First();
         string schemaName = reversedPath.Skip(1).First();
 
-        IStoredTableDataSet targetTableDataset = datasets
+        IStoredTableDataSet targetTableDataset = datasetList
             .First(x => x.Schema.Name.TextValue == schemaName)
             .First(x => x.Key.Name.TextValue == tableName)
             .Value;
 
         IQueryable<IRow> queryable = targetTableDataset;
+
+        if (query.Join is not null)
+        {
+            foreach (Join join in query.Join)
+            {
+                queryable = JoinApplicator.Apply(queryable, datasetList, join);
+            }
+        }
 
         if (query.Where is not null)
         {
@@ -71,8 +84,7 @@ internal sealed record RowsFromDatasets : IQueryable<IRow>
         }
 
         IEnumerable<IColumn> columns = query
-            .SelectExpressions.Select(x => x.AsT1.AsT4.AsT1)
-            .Select(x => new Column.Column(new String(x.Field), new StringColumnType()))
+            .SelectExpressions.Select(ColumnFromSelectExpression)
             .ToList();
 
         return queryable.Select(row =>
@@ -85,6 +97,41 @@ internal sealed record RowsFromDatasets : IQueryable<IRow>
                         c => new ColumnHash(c)
                     )
                 )
+        );
+    }
+
+    private static IColumn ColumnFromSelectExpression(SelectExpression expression)
+    {
+        if (expression.TryPickT0(out SingleValueReturning singleValue, out _))
+        {
+            return ColumnFromSingleValueReturning(singleValue);
+        }
+
+        if (expression.TryPickT1(out ArrayReturning arrayReturning, out _))
+        {
+            return ColumnFromArrayReturning(arrayReturning);
+        }
+
+        throw new NotSupportedException();
+    }
+
+    private static IColumn ColumnFromSingleValueReturning(SingleValueReturning _)
+    {
+        throw new NotSupportedException(
+            "SingleValueReturning (scalar/parameter) cannot be projected as a column field."
+        );
+    }
+
+    private static IColumn ColumnFromArrayReturning(ArrayReturning returning)
+    {
+        return returning.Match(
+            b => new Column.Column(new String(b.AsT1.Field), new BoolColumnType()),
+            d => new Column.Column(new String(d.AsT1.Field), new DateColumnType()),
+            dt => new Column.Column(new String(dt.AsT1.Field), new DateTimeColumnType()),
+            n => new Column.Column(new String(n.AsT1.Field), new DoubleColumnType()),
+            s => new Column.Column(new String(s.AsT1.Field), new StringColumnType()),
+            t => new Column.Column(new String(t.AsT1.Field), new TimeColumnType()),
+            u => new Column.Column(new String(u.AsT1.Field), new UuidColumnType())
         );
     }
 }
